efea5f13aaec5343b777abda53b789de14e9790d
anushkayadav <anushydv@gmail.com>
2019-05-22 23:24:36 +0530

    adding dragging components

---
+++ SRIP/exp5.js
@@ -0,0 +1,21 @@
+
+
+$(document).ready(function () {
+
+    $('.drop-zone').droppable({
+        accept: '.componentButton',
+        drop: function (event, ui) {
+            var $clone = ui.helper.clone();
+            if (!$clone.is('.inside-drop-zone')) {
+                $(this).append($clone.addClass('inside-drop-zone').draggable({
+                    containment: '.drop-zone'
+                }));
+            }
+        }
+    });
+
+    $('.componentButton').draggable({
+        helper: 'clone'
+    });
+
+});
